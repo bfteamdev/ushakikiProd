d052f1ce2e90e7b724b3f082e44af545fb050a2b
BorisHkl <borisvian68@gmail.com>
2021-03-26 08:29:34 +0200

    Update in header

--- resources/views/layout/partials/include/header.blade.php
+++ resources/views/layout/partials/include/header.blade.php
@@ -8,12 +8,9 @@
             </a>
     </div>
     <div class="header-right">
-        <div class="search">
-            <input type="search" class="searchInput" name="" id="" placeholder="Search.....">
-            <i class="fa fa-search"></i>
-        </div>
         <a class="MyAds" href="login.html"><i class="fa fa-list-ul"></i> My Ads</a>
         <a class="iconCart" href="login.html"><i class="flaticon2-shopping-cart-1"></i></a>
+        
         <div class="auth">
             <a class="authBtn" href="login.html">Login&nbsp;&nbsp;<i class="fas fa-user-circle"></i></a>
             <div class="separateAuth"></div>
